73caaba77c90a34e9ac1c15f01850d71839fd6cc
Eduardo Souza <Eduardodevtk@gmail.com>
2024-11-08 17:28:18 -0300

    Criando a contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,30 @@
-console.log('ola')
+AOS.init();
+
+//quando o evento iniciara primeiro: mês dia ano tempo
+const dataDoEvento = new Date("Dec 12, 2024, 19:00:00")
+//vamos pegar o de agora
+const timeStampDoEvento = dataDoEvento.getTime()
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date()
+    const timeStampAgora = agora.getTime()
+
+    const diasEvento = 1000 * 60 * 60 * 24;
+    const horasEventos = 1000 * 60 * 60;
+    const minutosEvento = 1000 * 60;
+    const segundosEvento = 1000
+
+    const distaciaAteEvento = timeStampDoEvento - timeStampAgora
+
+    const diasAteEvento = Math.floor(distaciaAteEvento / diasEvento)
+    const horasDoEvento = Math.floor(distaciaAteEvento % diasEvento / horasEventos)
+    const minutosDoEvento = Math.floor(distaciaAteEvento % horasEventos / minutosEvento)
+    const segundosDoEvento = Math.floor(distaciaAteEvento % minutosEvento / segundosEvento)
+
+    document.getElementById('contador').innerHTML = `${diasAteEvento}d ${horasDoEvento}h ${minutosDoEvento}m ${segundosDoEvento}s`
+
+    if (distaciaAteEvento < 0) {
+        clearInterval(contaAsHoras)
+        document.getElementById('contador').innerHTML = `Evento expirado`
+    }
+}, 1000)
